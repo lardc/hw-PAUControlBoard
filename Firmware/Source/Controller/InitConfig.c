59e9f1636ab6dbbb103c95624fd5ee293b5b1730
d.ivanov <d.ivanov@proton-electrotex.com>
2022-10-11 13:44:08 +0300

    Добавлена инициализация портов и внешних прерываний

--- Firmware/Source/Controller/InitConfig.c
+++ Firmware/Source/Controller/InitConfig.c
@@ -20,11 +20,31 @@ void INITCFG_ConfigIO()
 	
 	// Выходы
 	GPIO_InitPushPullOutput(GPIO_LED);
+	GPIO_InitPushPullOutput(GPIO_LED_EXT);
+	GPIO_InitPushPullOutput(GPIO_STST_CURRENT);
+	GPIO_InitPushPullOutput(GPIO_STST_MUX);
+	GPIO_InitPushPullOutput(GPIO_IGTU_MUX);
+	GPIO_InitPushPullOutput(GPIO_LCTU_MUX);
+	GPIO_InitPushPullOutput(GPIO_IDIV2);
+	//
+	GPIO_InitOpenDrainOutput(GPIO_IGTU_SEL, NoPull);
+	GPIO_InitOpenDrainOutput(GPIO_LCTU_SEL, NoPull);
+	GPIO_InitOpenDrainOutput(GPIO_IGTU_SI, NoPull);
+	GPIO_InitOpenDrainOutput(GPIO_LCTU_SI, NoPull);
 
-	// Входы
 
 	// Начальная установка состояний выводов
 	GPIO_SetState(GPIO_LED, false);
+	GPIO_SetState(GPIO_LED_EXT, false);
+	GPIO_SetState(GPIO_STST_CURRENT, false);
+	GPIO_SetState(GPIO_STST_MUX, false);
+	GPIO_SetState(GPIO_IGTU_SEL, false);
+	GPIO_SetState(GPIO_IGTU_MUX, false);
+	GPIO_SetState(GPIO_IGTU_SI, false);
+	GPIO_SetState(GPIO_LCTU_SEL, false);
+	GPIO_SetState(GPIO_LCTU_MUX, false);
+	GPIO_SetState(GPIO_LCTU_SI, false);
+	GPIO_SetState(GPIO_IDIV2, false);
 
 	// Альтернативные функции
 	GPIO_InitAltFunction(GPIO_ALT_UART1_RX, AltFn_7);
@@ -33,15 +53,18 @@ void INITCFG_ConfigIO()
 	GPIO_InitAltFunction(GPIO_ALT_CAN1_TX, AltFn_9);
 	GPIO_InitAltFunction(GPIO_ALT_SPI1_CLK, AltFn_5);
 	GPIO_InitAltFunction(GPIO_ALT_SPI1_DAT, AltFn_5);
+	GPIO_InitAltFunction(GPIO_ALT_UART2_TX, AltFn_7);
+	GPIO_InitAltFunction(GPIO_ALT_UART2_RX, AltFn_7);
 }
 
 //------------------------------------------------
 
 void INITCFG_ConfigExtInterrupt()
 {
-	// Вход SYNC
-	EXTI_Config(EXTI_PA, EXTI_6, FALL_TRIG, 0);
-	EXTI_EnableInterrupt(EXTI9_5_IRQn, 0, true);
+	EXTI_Init(EXTI_IGTU_SO, RISE_TRIG);
+	EXTI_Init(EXTI_LCTU_SO, RISE_TRIG);
+	EXTI_Init(EXTI_IGTU_SI, FALL_TRIG);
+	EXTI_Init(EXTI_LCTU_SI, FALL_TRIG);
 }
 //------------------------------------------------
 
