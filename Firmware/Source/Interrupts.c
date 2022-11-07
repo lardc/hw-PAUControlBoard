73424e758baa9557b2381fdd7e574611ad6bccb6
d.ivanov <d.ivanov@proton-electrotex.com>
2022-11-07 15:01:21 +0300

    Использована более короткая запись

--- Firmware/Source/Interrupts.c
+++ Firmware/Source/Interrupts.c
@@ -104,27 +104,27 @@ void INT_SyncInterruptProcess()
 bool INT_CheckSyncFromLCTU()
 {
 	FlagSyncFromLCTU = LL_CheckSyncFromLCTU();
-	return (DataTable[REG_CHANNEL] == CHANNEL_LCTU) ? FlagSyncFromLCTU : false;
+	return (DataTable[REG_CHANNEL] == CHANNEL_LCTU) && FlagSyncFromLCTU;
 }
 //-----------------------------------------
 
 bool INT_CheckSyncFromIGTU()
 {
 	FlagSyncFromIGTU = LL_CheckSyncFromIGTU();
-	return (DataTable[REG_CHANNEL] == CHANNEL_IGTU) ? FlagSyncFromIGTU : false;
+	return (DataTable[REG_CHANNEL] == CHANNEL_IGTU) && FlagSyncFromIGTU;
 }
 //-----------------------------------------
 
 bool INT_CheckSyncToLCTU()
 {
 	FlagSyncToLCTU = LL_CheckSyncToLCTU();
-	return (DataTable[REG_CHANNEL] == CHANNEL_LCTU) ? FlagSyncToLCTU : false;
+	return (DataTable[REG_CHANNEL] == CHANNEL_LCTU) && FlagSyncToLCTU;
 }
 //-----------------------------------------
 
 bool INT_CheckSyncToIGTU()
 {
 	FlagSyncToIGTU = LL_CheckSyncToIGTU();
-	return (DataTable[REG_CHANNEL] == CHANNEL_IGTU) ? FlagSyncToIGTU : false;
+	return (DataTable[REG_CHANNEL] == CHANNEL_IGTU) && FlagSyncToIGTU;
 }
 //-----------------------------------------
